1d6c08126b4a9128c203ab4e60e9e0dd0a0288ad
Dominik Diessner <dominik.diessner@gmx.de>
2024-11-20 15:10:18 +0100

    Updated Todo in Readme + Removed flask

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Bandcorder
 
 description: TODO
+
+
+Todo:
+
+- 1 Mobile UI- Flutter
+- 2 Reconfiguration as yml file
+  - 2.1 Specify recording directory
+- 3 Desktop UI Tkinter
+- 4 Replace Flask with socket.io server
+- 5 Create virtualenv
+- 6 Create Release.exe
+
+ 
--- server/app.py
+++ server/app.py
@@ -1,53 +1,55 @@
 import logging
+import eventlet
+import socketio
 
-from flask import Flask
-from flask_socketio import SocketIO, emit
 from recorder import Recorder
 
 recorder = Recorder()
-app = Flask(__name__)
-socketio = SocketIO(app)
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
+
+
 logger = logging.getLogger(__name__)
 
-@socketio.on('connect')
-def test_connect():
-    logger.info('Client connected')
+ #@sio.on('connect')
+#def test_connect():
+#   logger.info('Client connected')
 
-@socketio.on('disconnect')
-def test_disconnect():
-    logger.info('Client disconnected')
+#@sio.on('disconnect')
+#def test_disconnect():
+#    logger.info('Client disconnected')
 
-@socketio.on('StartRecording')
-def start_recording():
+@sio.on('StartRecording')
+def start_recording(event):
     logger.info('Received request to start recording')
     try:
         recorder.start()
-        emit('RecordingStateChange', { 'recording': True }, broadcast=True)
+        sio.emit('RecordingStateChange', { 'recording': True })
         logger.info('Recording was started successfully')
     except Exception as e:
         logger.error('Failed to start recording')
-        emit('RecordingStateChangeFailed', {
+        sio.emit('RecordingStateChangeFailed', {
             'requestedState': True,
             'reason': str(e)
-        }, broadcast=True)
+        })
 
-@socketio.on('StopRecording')
-def stop_recording():
+@sio.on('StopRecording')
+def stop_recording(event):
     try:
         recorder.stop()
-        emit('RecordingStateChange', { 'recording': False }, broadcast=True)
+        sio.emit('RecordingStateChange', { 'recording': False })
         logger.info('Recording was stopped successfully')
     except Exception as e:
         logger.error('Failed to stop recording')
-        emit('RecordingStateChangeFailed', {
+        sio.emit('RecordingStateChangeFailed', {
             'requestedState': False,
             'reason': str(e)
-        }, broadcast=True)
+        })
 
-@socketio.on('QueryRecordingState')
+@sio.on('QueryRecordingState')
 def query_recording_state():
     is_recording = recorder.get_is_recording()
-    emit('RecordingState', { 'isRecording': is_recording })
+    sio.emit('RecordingState', { 'isRecording': is_recording })
 
 if __name__ == '__main__':
-    socketio.run(app)
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
