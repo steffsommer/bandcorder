07143099eac9c328c195da27a3711c1f5b56f49f
Stefan Sommer <stefan.sommer.per@gmail.com>
2025-07-26 17:50:18 +0200

    added header

--- server/frontend/src/components/card/card.tsx
+++ server/frontend/src/components/card/card.tsx
@@ -2,7 +2,9 @@ import { ComponentProps } from "react";
 import "./card.css";
 
 export function Card(props: ComponentProps<"div">) {
-  return <div className="card">
+  return (
+    <div className={"card shadow rounded " + props.className}>
       {props.children}
-  </div>;
+    </div>
+  );
 }
--- server/frontend/src/components/header/header.tsx
+++ server/frontend/src/components/header/header.tsx
@@ -1,5 +1,18 @@
+import { Card } from "../card/card";
 import "./header.css";
+import logo from "../../assets/images/logo.svg";
+import { FiSettings } from "react-icons/fi";
 
 export function Header() {
-  return <header>hi</header>;
+  return (
+    <header>
+      <Card className="logo">
+        <img className="logo-img" src={logo} />
+        <h1>BANDCORDER</h1>
+      </Card>
+      <Card className="centered">
+        <FiSettings size="2em" />
+      </Card>
+    </header>
+  );
 }
