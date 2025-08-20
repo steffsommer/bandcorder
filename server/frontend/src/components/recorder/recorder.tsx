17b299b167cfbec6531ca04855cb37e6b727caed
Stefan Sommer <stefan.sommer.per@gmail.com>
2025-08-20 23:00:38 +0200

    recorder component layout

--- server/frontend/src/components/recorder/recorder.tsx
+++ server/frontend/src/components/recorder/recorder.tsx
@@ -1,3 +1,4 @@
+import { Card } from "../card/card";
 import "./recorder.css";
 import { Spinner } from "./spinner/spinner";
 
@@ -5,7 +6,17 @@ export const Recorder: React.FC = () => {
   return (
     <div className="recorder">
       <h2 className="descriptive-header">Recorder</h2>
-      <Spinner isRecording={true} />
+      <Spinner isRecording={true} className="spinner" />
+      <div className="current-file">
+        <span>current file info</span>
+      </div>
+      <Card className="frequency-card">
+        <span>frequency info</span>
+      </Card>
+      <Card className="volume-card">
+        <span>volume info</span>
+      </Card>
+      <div className="controls">controls</div>
     </div>
   );
 };
--- server/frontend/src/components/recorder/spinner/spinner.tsx
+++ server/frontend/src/components/recorder/spinner/spinner.tsx
@@ -2,11 +2,17 @@ import "./spinner.css";
 
 interface Props {
   isRecording: boolean;
+  className: string;
 }
 
-
-export const Spinner: React.FC<Props> = ({isRecording}) => {
+export const Spinner: React.FC<Props> = ({ isRecording, className }) => {
   return (
-    <div className={"spinner " + (isRecording ? "active" : "")}></div>
+    <div
+      className={
+        "spinner " +
+        (isRecording ? "active" : "") +
+        (className ? " " + className : "")
+      }
+    ></div>
   );
 };
