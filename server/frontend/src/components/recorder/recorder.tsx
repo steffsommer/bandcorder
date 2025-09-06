9060c0a0750e44d8f62d48b30ad2b6c5d2c9e816
Stefan Sommer <stefan.sommer.per@gmail.com>
2025-09-06 13:24:48 +0200

    display file name of current recording

--- server/frontend/src/components/recorder/recorder.tsx
+++ server/frontend/src/components/recorder/recorder.tsx
@@ -1,22 +1,48 @@
-import { Card } from "../card/card";
-import { Button } from "../button/button";
-import "./recorder.css";
-import { Timer } from "./timer/timer";
+import { useEffect, useState } from "react";
 import { FaFile, FaPause, FaPlay, FaSquareFull } from "react-icons/fa";
 import {
   Abort,
   Start,
   Stop,
 } from "../../../wailsjs/go/facades/RecordingFacade.js";
+import { EventsOn } from "../../../wailsjs/runtime/runtime.js";
+import { Button } from "../button/button";
+import { Card } from "../card/card";
+import {
+  EventID,
+  RecordingRunningEvent,
+  RecordingState,
+  RecordingStateEvent,
+} from "../events.js";
+import "./recorder.css";
+import { Timer } from "./timer/timer";
 
 export const Recorder: React.FC = () => {
+  const [recordingName, setRecordingName] = useState("");
+
+  useEffect(() => {
+    return EventsOn(EventID.RecordingState, (ev: RecordingStateEvent<any>) => {
+      if (ev.State === RecordingState.RUNNING) {
+        const runningEvent = ev as RecordingRunningEvent;
+        setRecordingName(runningEvent.FileName);
+      } else {
+        setRecordingName("");
+      }
+    });
+  });
+
   return (
     <div className="recorder">
       <h2 className="heading">Recorder</h2>
       <Timer className="timer-widget" />
-      <div className="current-file">
+      <div
+        className="current-file"
+        style={
+          recordingName === "" ? { visibility: "hidden" } : { visibility: "visible" }
+        }
+      >
         <FaFile size="1.2em" className="file-icon" />
-        <h3>2025-08-21--19-45-00.wav</h3>
+        <h3>{recordingName}</h3>
       </div>
       <Card className="frequency-card">
         <span>🚧 frequency info 🚧</span>
