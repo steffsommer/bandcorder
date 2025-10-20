c3236767f67333483ffa11dcf3036ffa884c78f2
Stefan Sommer <stefan.sommer.per@gmail.com>
2025-10-20 19:23:17 +0200

    added modal for file renaming

--- server/frontend/src/components/recordings-list/recordings-list-entry/recordings-list-entry.tsx
+++ server/frontend/src/components/recordings-list/recordings-list-entry/recordings-list-entry.tsx
@@ -6,12 +6,15 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 import { Button } from "../../button/button";
 import { models } from "../../../../wailsjs/go/models";
 import { TimeUtils } from "../../../utils/time";
+import { RenameModal } from "./rename-modal/rename-modal";
+import { useState } from "react";
 
 interface Props {
   recording: models.RecordingInfo;
 }
 
 export const RecordingsListEntry: React.FC<Props> = ({ recording }) => {
+  const [showRenameModal, setShowRenameModal] = useState(false);
   return (
     <Card className="entry-card">
       <div className="row">
@@ -21,17 +24,20 @@ export const RecordingsListEntry: React.FC<Props> = ({ recording }) => {
         <h3 className="recording-title">{recording.fileName}</h3>
         <span className="duration">
           <MdOutlineAccessTimeFilled size="2em" />
-          <span className="duration-str">
-            {TimeUtils.toMMSS(recording.durationSeconds)}
-          </span>
+          <span className="duration-str">{TimeUtils.toMMSS(recording.durationSeconds)}</span>
         </span>
-        <Button className="list-btn edit-btn">
+        <Button className="list-btn edit-btn" onClick={() => setShowRenameModal(true)}>
           <FaEdit />
         </Button>
         <Button className="list-btn">
           <FaTrash />
         </Button>
       </div>
+      <RenameModal
+        show={showRenameModal}
+        initialFileValue={recording.fileName}
+        onClose={() => setShowRenameModal(false)}
+      />
     </Card>
   );
 };
---
+++ server/frontend/src/components/recordings-list/recordings-list-entry/rename-modal/rename-modal.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useRef } from "react";
+import { FiFile, FiSave } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
+import { Button } from "../../../button/button";
+import "./rename-modal.css";
+
+interface Props {
+  show?: boolean;
+  onClose: () => void;
+  initialFileValue: string;
+}
+
+export function RenameModal({ show, onClose, initialFileValue }: Props) {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (show) {
+      if (inputRef.current) {
+        inputRef.current.value = initialFileValue;
+      }
+      dialogRef?.current?.showModal();
+    } else {
+      dialogRef?.current?.close();
+    }
+  }, [show]);
+
+  const handleSubmit = async () => {
+    console.log("initial: " + initialFileValue + ", now: " + inputRef.current?.value);
+  };
+
+  return (
+    <dialog ref={dialogRef} className="rename-dialog" onClose={onClose}>
+      <form method="dialog" onSubmit={handleSubmit}>
+        <h2 className="rename-heading">
+          <FiFile />
+          <span>Rename file</span>
+          <button type="button" className="close-btn" onClick={() => dialogRef?.current?.close()}>
+            <IoMdClose className="rename-close-icon" />
+          </button>
+        </h2>
+        <div className="rename-list">
+          <label htmlFor="fileName">File name</label>
+          <input autoFocus ref={inputRef} id="file-name" name="fileName" />
+        </div>
+        <Button className="save-btn">
+          <FiSave />
+          <span>Save</span>
+        </Button>
+      </form>
+    </dialog>
+  );
+}
