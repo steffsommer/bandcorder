cc68cea0789726fb8332e55e354040a09f1572bf
Stefan Sommer <stefan.sommer.per@gmail.com>
2025-11-03 22:56:06 +0100

    added router and nav items

--- server/frontend/src/App.tsx
+++ server/frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "./App.css";
 import { Card } from "./components/card/card";
@@ -7,6 +8,18 @@ import { IP } from "./components/ip/ip";
 import { Recorder } from "./components/recorder/recorder";
 import { RecordingsList } from "./components/recordings-list/recordings-list";
 import { SettingsModal } from "./components/settings-modal/settings-modal";
+import MetronomePage from "./pages/metronome-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RecordingPage />,
+  },
+  {
+    path: "/metronome",
+    element: <MetronomePage />,
+  },
+]);
 
 function App() {
   const [showSettings, setShowSettings] = useState(false);
@@ -14,18 +27,23 @@ function App() {
     <div id="app">
       <Header onSettingsClick={() => setShowSettings(true)} />
       <div className="container">
-        <div className="recording-layout">
-          <Card>
-            <RecordingsList />
-          </Card>
-          <Card>
-            <Recorder />
-          </Card>
-        </div>
-        <IP className="ip" />
+        <RouterProvider router={router} />;
+        <SettingsModal show={showSettings} onClose={() => setShowSettings(false)} />
+        <ToastContainer autoClose={2000} />
       </div>
-      <SettingsModal show={showSettings} onClose={() => setShowSettings(false)} />
-      <ToastContainer autoClose={2000} />
+    </div>
+  );
+}
+
+function RecordingPage() {
+  return (
+    <div className="recording-layout">
+      <Card>
+        <RecordingsList />
+      </Card>
+      <Card>
+        <Recorder />
+      </Card>
     </div>
   );
 }
--- server/frontend/src/components/header/header.tsx
+++ server/frontend/src/components/header/header.tsx
@@ -3,18 +3,34 @@ import "./header.css";
 import logo from "../../assets/images/logo.svg";
 import { FiSettings } from "react-icons/fi";
 import { Button } from "../button/button";
+import { PiMicrophoneStageBold } from "react-icons/pi";
+import { TbMetronome } from "react-icons/tb";
 
 interface Props {
   onSettingsClick: () => void;
 }
 
-export function Header({onSettingsClick}: Props) {
+export function Header({ onSettingsClick }: Props) {
+  const currentPath = window.location.pathname;
+
   return (
     <header>
       <Card className="logo">
-        <img className="logo-img" src={logo} />
-        <h1>BANDCORDER</h1>
+        <a href="/" className="logo">
+          <img className="logo-img" src={logo} />
+          <h1>BANDCORDER</h1>
+        </a>
       </Card>
+      <div className="nav-items">
+        <a href="/" className={`nav-item ${currentPath === "/" ? "active" : ""}`}>
+          <PiMicrophoneStageBold size="1.3em" />
+          <span>RECORD</span>
+        </a>
+        <a href="/metronome" className={`nav-item ${currentPath === "/metronome" ? "active" : ""}`}>
+          <TbMetronome size="1.5em" />
+          <span>METRONOME</span>
+        </a>
+      </div>
       <Button className="settings-btn" onClick={onSettingsClick}>
         <FiSettings size="2em" />
       </Button>
